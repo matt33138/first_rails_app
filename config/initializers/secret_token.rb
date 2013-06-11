e916178f1c718aa689673e1f77f71caa0f69e104
Matt Coffman <mattcoff@gmail.com>
2013-06-11 17:02:22 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Junerailstutorial::Application.config.secret_token = 'bac5c29a4f52175b6a24fb36d463b5baf87e2001102b69565c8f48612aadf273261feab0f0a33899f0c20915f54b2ae9c707b11733f6c5ecbd27c423549e67a3'
